10afebb4d954ab44ec63a32bcf163f36c3834973
lizparody <lizparody@gmail.com>
2017-09-28 16:09:08 -0500

    Renamed the files

---
+++ 04-check_zero.exs
@@ -0,0 +1,22 @@
+defmodule Math do
+  def zero?(0) do
+    true
+  end
+
+  def zero?(x) when is_integer(x) do
+    false
+  end
+end
+
+IO.puts Math.zero?(0)         #=> true
+IO.puts Math.zero?(1)         #=> false
+#IO.puts Math.zero?([1, 2, 3]) #=> ** (FunctionClauseError)
+#IO.puts Math.zero?(0.0)       #=> ** (FunctionClauseError)
+
+#shorter version:
+"""
+defmodule Math do
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
+end
+"""
--- check_zero.exs
+++
@@ -1,9 +0,0 @@
-defmodule Math do
-  def zero?(0) do
-    true
-  end
-
-  def zero?(x) when is_integer(x) do
-    false
-  end
-end
