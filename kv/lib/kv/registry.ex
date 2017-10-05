56f3b4e46792fb5678b5f6fa0a6f661a001eb4dd
lizparody <lizparody@gmail.com>
2017-10-05 09:53:31 -0500

    added the registry and registry test

---
+++ kv/lib/kv/registry.ex
@@ -0,0 +1,79 @@
+defmodule KV.Registry do
+  use GenServer
+
+  ## Client API
+
+  @doc """
+  Starts the registry.
+  """
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts) # 3 args, 1)current module, 2)The initialization arguments 3)A list of options which can be used to specify things like the name of the server
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
+  def lookup(server, name) do # responsible to send request to the server
+    GenServer.call(server, {:lookup, name}) # Calls are synchronous and the server must send a response back to such requests.
+  end
+
+  @doc """
+  Ensures there is a bucket associated with the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name}) # Casts are asynchronous and the server won’t send a response back.
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:lookup, name}, _from, names) do # it matches the lookup function
+    {:reply, Map.fetch(names, name), names}
+  end
+
+  def handle_cast({:create, name}, names) do # it matches the create function
+    if Map.has_key?(names, name) do
+      {:noreply, names}
+    else
+      {:ok, bucket} = KV.Bucket.start_link([])
+      {:noreply, Map.put(names, name, bucket)}
+    end
+  end
+
+  def init(:ok) do
+    names = %{}
+    refs  = %{}
+    {:ok, {names, refs}}
+  end
+
+  def handle_call({:lookup, name}, _from, {names, _} = state) do
+    {:reply, Map.fetch(names, name), state}
+  end
+
+  def handle_cast({:create, name}, {names, refs}) do
+    if Map.has_key?(names, name) do
+      {:noreply, {names, refs}}
+    else
+      {:ok, pid} = KV.Bucket.start_link([])
+      ref = Process.monitor(pid)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, pid)
+      {:noreply, {names, refs}}
+    end
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+end
